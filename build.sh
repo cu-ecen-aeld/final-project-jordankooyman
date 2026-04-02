a07ce1dd51b9f614f08fabe56d14e2edef42d658
jordankooyman <jordankooyman@gmail.com>
2026-04-02 09:16:20 -0600

    Buildroot Coonfigs V1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Script to build buildroot configuration
-#Author: Siddhant Jajoo
+#Author: Siddhant Jajoo, modified for the RPi 4 by Jordan Kooyman
 
 source shared.sh
 
@@ -14,20 +14,17 @@ cd `dirname $0`
 
 if [ ! -e buildroot/.config ]
 then
-	echo "MISSING BUILDROOT CONFIGURATION FILE"
-
-	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
-	then
-		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
-	else
-		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
-		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
-	fi
+    echo "MISSING BUILDROOT CONFIGURATION FILE"
+    if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
+    then
+        echo "USING ${AESD_MODIFIED_DEFCONFIG}"
+        make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} \
+            BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+    else
+        echo "Run ./save_config.sh to save this as the default configuration"
+        make -C buildroot raspberrypi4_64_defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
+    fi
 else
-	echo "USING EXISTING BUILDROOT CONFIG"
-	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
-
+    echo "USING EXISTING BUILDROOT CONFIG"
+    make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
 fi
