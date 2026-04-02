8c495a3dd9c769c61c995fae413126fbebffdf52
jordankooyman <jordankooyman@gmail.com>
2026-04-02 14:09:43 -0600

    Image Creation Fixes; Tested: Image does not boot

--- base_external/board/rpi4/post-image.sh
+++ base_external/board/rpi4/post-image.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
+set -e
+
+# Copy our custom config.txt into the firmware directory
 cp "$BR2_EXTERNAL_SPI_DISPLAY_PATH/board/rpi4/config.txt" "$BINARIES_DIR/rpi-firmware/config.txt"
+
+# Call the standard Buildroot RPi4 post-image script to run genimage
+# and assemble the final sdcard.img
+"$BR2_EXTERNAL_SPI_DISPLAY_PATH/../buildroot/board/raspberrypi4-64/post-image.sh" "$@"
