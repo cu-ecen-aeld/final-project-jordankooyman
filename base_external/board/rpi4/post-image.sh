0bab43c265774e71755c8cf17b9d5d303395e595
jordankooyman <jordankooyman@gmail.com>
2026-04-02 13:03:44 -0600

    Fix variable error in post-image.sh

--- base_external/board/rpi4/post-image.sh
+++ base_external/board/rpi4/post-image.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-cp "$BR2_EXTERNAL_EXT_PATH/board/rpi4/config.txt" "$BINARIES_DIR/rpi-firmware/config.txt"
-
+cp "$BR2_EXTERNAL_SPI_DISPLAY_PATH/board/rpi4/config.txt" "$BINARIES_DIR/rpi-firmware/config.txt"
