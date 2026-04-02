324720cec3bc912ca7df6641d132434b73061628
jordankooyman <jordankooyman@gmail.com>
2026-04-02 12:55:17 -0600

    Update buildroot submodule to libgpiod v2.1.3, fix ili9488 build flags

--- base_external/package/ili9488/ili9488.mk
+++ base_external/package/ili9488/ili9488.mk
@@ -1,4 +1,4 @@
-ILI9488_VERSION = 79fa722
+ILI9488_VERSION = adb8c24
 ILI9488_SITE = git@github.com:jordankooyman/ili9488-userspace-driver.git
 ILI9488_SITE_METHOD = git
 ILI9488_GIT_SUBMODULES = YES
@@ -6,11 +6,14 @@ ILI9488_GIT_SUBMODULES = YES
 ILI9488_DEPENDENCIES = libgpiod
 
 define ILI9488_BUILD_CMDS
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/demo
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) \
+        CFLAGS="$(TARGET_CFLAGS) -I../include" \
+        LDFLAGS="$(TARGET_LDFLAGS) -lgpiod" \
+        -C $(@D)/demo
 endef
 
 define ILI9488_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/demo/ili9488-demo $(TARGET_DIR)/usr/bin/ILI9488-demo
+    $(INSTALL) -D -m 0755 $(@D)/demo/ili9488_demo $(TARGET_DIR)/usr/bin/ILI9488-demo
 endef
 
 $(eval $(generic-package))
